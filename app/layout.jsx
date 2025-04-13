8ade78380eb7eb5868ae4b1fd1d51334f41a1efd
MichaelVenturi <michaelventuri58@aol.com>
2025-04-13 15:53:51 -0400

    metadata + asset setup

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import "@/assets/styles/globals.css";
+
+// favicon is alo loaded in automatically, just by having a file called "favicon.ico" placed in the app folder
+
+// this metadata is automatically loaded in just by exporting it here in the layout
+export const metadata = {
+  title: "Property Pulse",
+  // key words rendered on server instead of on the client, improving SEO
+  // (search engine optimization, search engines can find these keywords more easily)
+  keywords: "rental, property, real estate",
+  description: "Find the perfect rental property",
+};
+
+const mainLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+export default mainLayout;
---
+++ app/page.jsx
@@ -0,0 +1,4 @@
+const HomePage = () => {
+  return <div className="text-2xl font-display">HomePage</div>;
+};
+export default HomePage;
