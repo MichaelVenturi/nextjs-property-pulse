56ffdbcee98dc751d69460b9e16b2fc09d4264f0
MichaelVenturi <michaelventuri58@aol.com>
2025-04-14 20:39:33 -0400

    get properties + property details page

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -5,18 +5,42 @@
 // "use client"; // this line indicates to make this a client componnet
 // import { useRouter, useParams, useSearchParams } from "next/navigation"; //make sure not to import from next/router, which is for the pages directory (old way)
 
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import PropertyDetails from "@/components/PropertyDetails";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
+
 // in next 15, params and searchparams are async.  So on client, you need the react "use" hook
 // on server, you make the component async and await params (cant make client components async)
 // im not really sure what the difference is between using the hooks or not on client side.  The hooks seem to still work
 const PropertyPage = async ({ params, searchParams }) => {
-  const p = await params;
+  await connectDB();
+
+  const { id } = await params;
   const sp = await searchParams;
+
+  const property = await Property.findById(id).lean();
   return (
-    <div>
-      <div>
-        {p.id} and {sp.name}
-      </div>
-    </div>
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link href="/properties" className="text-blue-500 hover:text-blue-600 flex items-center">
+            <FaArrowLeft className="mr-2" /> Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <PropertyDetails property={property} />
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 export default PropertyPage;
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,14 @@
-import properties from "@/properties.json";
 import PropertyCard from "@/components/PropertyCard";
-const PropertiesPage = () => {
-  console.log(properties);
+// here is where Nextjs shines:  no need for a separate backend with routes and handlers, and then having to do a useEffect in the component to hit those routes
+import connectDB from "@/config/database"; // we can just import our connectDB function and the models we want to work with and use them directly in our server component
+import Property from "@/models/Property"; // this is great, because personally I hate useEffect
+
+// as such, our server component will need to be async to use things like connectDB (client components cannot be async)
+const PropertiesPage = async () => {
+  // this right here is amazing.  Two lines and you just fetched all your data, right from within the component.  Eat shit, useEffect
+  await connectDB();
+  // .lean() returns plain javascript objects instead of mongoose documents, optimizing performance.  This means the results will be read only though
+  const properties = await Property.find({}).lean();
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
