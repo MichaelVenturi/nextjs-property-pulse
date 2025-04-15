eadc0e6d2dc45b4639ddf0afb67fef445ec91642
MichaelVenturi <michaelventuri58@aol.com>
2025-04-15 17:31:05 -0400

    add property works

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -6,9 +6,26 @@
 
 // since these are ran on the server, notice that any console logs will only appear in the terminal, not on the browser
 
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache"; // once we submit, updates the cache and the listings so what we just added will show up
+import { redirect } from "next/navigation";
+
 // this action gets applied right to the form tag and will be triggered on submit
 // server actions use the POST method.  Notice that when this action runs, the terminal will say POST followed by the URL it occurred from (POST /properties/add)
 const addProperty = async (formData) => {
+  await connectDB();
+
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser || !sessionUser.userId) {
+    // when throwing an error, it will open error.jsx, once again a page that will automatically show up where it needs to so long as we name it and place it correctly
+    throw new Error("User ID is required");
+  }
+
+  const { userId } = sessionUser; // this will be the mongo _id for the user currently logged in
+
   // format data
   // access all values from amenities
   const amenities = formData.getAll("amenities"); //these tags come from the name property for the input fields in the form
@@ -19,6 +36,7 @@ const addProperty = async (formData) => {
     .map((image) => image.name); // return an array of just the image names
 
   const propertyData = {
+    owner: userId,
     type: formData.get("type"),
     name: formData.get("name"),
     description: formData.get("description"),
@@ -44,7 +62,12 @@ const addProperty = async (formData) => {
     },
     images,
   };
-  console.log(propertyData);
+
+  const newProperty = new Property(propertyData);
+  await newProperty.save();
+
+  revalidatePath("/", "layout");
+  redirect(`/properties/${newProperty._id}`);
 };
 
 export default addProperty;
