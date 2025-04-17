9cbcf87744233de90632906746f2f33c79408711
MichaelVenturi <michaelventuri58@aol.com>
2025-04-16 21:11:25 -0400

    bookmark button ui

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,12 +1,35 @@
 "use client";
+import { useState, useEffect } from "react";
 import bookmarkProperty from "@/app/actions/bookmarkProperty";
+import checkBookmarkStatus from "@/app/actions/checkBookmarkStatus";
 import { toast } from "react-toastify";
 import { useSession } from "next-auth/react";
 import { FaBookmark } from "react-icons/fa";
 
 const BookmarkButton = ({ property }) => {
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [loading, setLoading] = useState(true);
+
   const { data: session } = useSession();
   const userId = session?.user?.id;
+
+  useEffect(() => {
+    if (!userId) {
+      setLoading(false);
+      return;
+    }
+
+    checkBookmarkStatus(property._id).then((res) => {
+      if (res.error) {
+        toast.error(res.error);
+      }
+      if (res.isBookmarked) {
+        setIsBookmarked(res.isBookmarked);
+      }
+      setLoading(false);
+    });
+  }, [property._id, userId, checkBookmarkStatus]);
+
   const handleClick = async () => {
     if (!userId) {
       toast.error("You need to be signed in to bookmark a listing");
@@ -17,10 +40,25 @@ const BookmarkButton = ({ property }) => {
       if (res.error) {
         return toast.error(res.error);
       }
+      setIsBookmarked(res.isBookmarked);
       toast.success(res.message);
     });
   };
-  return (
+
+  if (loading) {
+    return (
+      <button className="bg-gray-400 text-white text-center font-bold w-full py-2 px-4 rounded-full flex items-center justify-center" disabled>
+        Loading...
+      </button>
+    );
+  }
+  return isBookmarked ? (
+    <button
+      className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+      onClick={handleClick}>
+      <FaBookmark className="mr-2" /> Remove Bookmark
+    </button>
+  ) : (
     <button
       className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
       onClick={handleClick}>
